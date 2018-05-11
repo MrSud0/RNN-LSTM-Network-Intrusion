b71e67fdc3eb840b4e04021865019feecb44e281
Anihilakos <jason_somarakis@hotmail.com>
2018-05-11 15:11:10 +0300

    Update FeatureSelector.sh

--- FeatureSelector.sh
+++ FeatureSelector.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
-#this scripts runs a feature extractor located in my machine and outputs a csv file with all the features extracted ,shape[samples,features]
-
 figlet "Feature Extractor"
-
 printf "Operation Feature Extraction\n"
-printf "Getting pcap file \n"
 INPUT_PCAP=$1
-wc $INPUT_PCAP
-
-printf "Getting Headers \n"
 HEADERS_CSV=$2
-wc $HEADERS_CSV
+if [ -z $2 ] 
+then
+	printf "proceeding without headers\n"
+	printf "Getting pcap file\n"
+	wc $INPUT_PCAP
+	/home/anihilakos/CLionProjects/kdd99_feature_extractor/cmake-build-debug/src/kdd99extractor $INPUT_PCAP >> /home/anihilakos/FeatureExtrFiles/output.csv
+	printf "Creating finalOutput.csv at /home/anihilakos/FeatureExtrFiles \n\n" 
+	cat /home/anihilakos/FeatureExtrFiles/output.csv > /home/anihilakos/FeatureExtrFiles/finalOutput.csv
+	rm /home/anihilakos/FeatureExtrFiles/output.csv
+else
+	printf "proceeding with headers\n"
+	printf "Getting pcap file\n"
+	wc $INPUT_PCAP
+	printf "Getting Headers \n"
+	wc $HEADERS_CSV
+	printf "Creating finalOutput.csv at /home/anihilakos/FeatureExtrFiles \n\n" 
+	/home/anihilakos/CLionProjects/kdd99_feature_extractor/cmake-build-debug/src/kdd99extractor -e $INPUT_PCAP >> /home/anihilakos/FeatureExtrFiles/output.csv
+	cat $HEADERS_CSV /home/anihilakos/FeatureExtrFiles/output.csv > /home/anihilakos/FeatureExtrFiles/finalOutput.csv
+	rm /home/anihilakos/FeatureExtrFiles/output.csv
+fi
 
-printf "Creating finalOutput.csv at /home/anihilakos/FeatureExtrFiles \n\n"
-/home/anihilakos/CLionProjects/kdd99_feature_extractor/cmake-build-debug/src/kdd99extractor -e $INPUT_PCAP >> /home/anihilakos/FeatureExtrFiles/output.csv
-rm output.csv
-cat $HEADERS_CSV /home/anihilakos/FeatureExtrFiles/outputpart1.csv > /home/anihilakos/FeatureExtrFiles/finalOutput.csv
+
+
+
+
+
+
+
